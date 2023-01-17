b89f6a9bb717c0d8d4ac9ba4cd722f5774cc03b8
LeeJongAnn <dlwhddksiq@innotree.com>
2023-01-17 15:07:42 +0900

    Initial commit - 스프링 핵심 원리 기본편

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,16 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    void join() {
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        member
+    }
+
+}
